5230b2e1ec28ef5a3d416981a0ce4f0a752fcd8b
Administrator <admin@example.com>
2020-03-12 13:37:09 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <h4 class="d-flex align-center">Planification Experimentation</h4>
+      <template v-slot:extension>
+        <v-col cols="3">
+          <v-text-field  v-model="ligne" label="Ligne" outlined></v-text-field>
+        </v-col>
+        <v-col cols="3">
+          <v-text-field  v-model="colonne" label="Colonne" outlined></v-text-field>
+        </v-col>
+        <v-col>
+          <v-btn @click="generate()">Generer</v-btn>
+        </v-col>
+      </template>
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld :colonne="colonneSelected" :ligne="ligneSelected" class="tableau" />
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'app',
+  name: "App",
+
   components: {
     HelloWorld
+  },
+
+  data: () => ({
+    ligne: null,
+    colonne: null,
+    ligneSelected:null,
+    colonneSelected:null
+  }),
+  methods:{
+    generate(){
+      this.ligneSelected = parseInt(this.ligne)
+      this.colonneSelected = parseInt(this.colonne)
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style >
+.tableau {
+  margin-top: 10em;
 }
 </style>
