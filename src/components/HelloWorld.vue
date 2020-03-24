18a558b77a92c53b66f53304ebaf4399002bb9f8
Administrator <admin@example.com>
2020-03-24 19:44:43 +0100

    modif colonne / ligne

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,13 +4,14 @@
       <v-col>
         <table>
           <tbody>
-            <tr v-for="(colonne,j) in colonne" :key="j">
-              <td v-for="(ligne,i) in ligne" :key="i">R{{j+1}}m{{i+1}}</td>
+            <tr v-for="(ligne,j) in ligne" :key="j">
+              <td v-for="(colonne ,i) in colonne" :key="i">R{{j+1}}m{{i+1}}</td>
             </tr>
           </tbody>
         </table>
       </v-col>
     </v-row>
+    <v-btn color="primary" @click="melanger">Melanger</v-btn>
   </v-container>
 </template>
 
@@ -22,7 +23,18 @@ export default {
     colonne:Number
   },
   data: () => ({
-  })
+    tableau:[]
+  }),
+  methods:{
+    melanger(){
+      let taille = this.ligne * this.colonne
+      console.log(taille)
+      for(let i=1; i< taille +1  ; i++){
+        //let ligne = "R"++"M"+i
+        console.log("R"+i+"M"+i)
+      }
+    }
+  }
 };
 </script>
 
